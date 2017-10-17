521806475e4beee0f05f0be5f5fd41df2f7362da
phuong.dao <phuong.dao@dwork.vn>
2017-10-17 10:34:46 +0700

    add playListDetailScreen ( 80%) - add playlist related api

--- app/reducers/apiReducer.js
+++ app/reducers/apiReducer.js
@@ -6,7 +6,8 @@ export default (state, action) => {
     let newState = state
     if (isResponse(type)) {
         if (isResponseOk(type)) {
-            newState = newState.merge(action.payload.response.entities,{deep: true})
+            newState = newState.merge(action.payload.response.entities,{deep: true});
+            newState = newState["\"playlistRelation\""] !== undefined ? [] : newState;
             newState = newState.merge({[JSON.stringify(action.payload.request.schemaName)]: action.payload.response.result})
         }
     }
