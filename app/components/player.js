b31a9ae919e306ea70083924ff85740b88b39b5d
Phuc Nguyen <phuc.nguyen@dwork.vn>
2017-10-26 15:29:15 +0700

    fix style touchable button

--- app/components/player.js
+++ app/components/player.js
@@ -1,11 +1,11 @@
 import {connect} from "react-redux"
 import {PLAYER_NOWLIST_NEXT, PLAYER_PAUSE, PLAYER_PLAY, PLAYER_TOGGLE, PLAYER_NOWLIST_CLEAR, PLAYER_NOWLIST_ADD, PLAYER_UPDATE_TIME} from "../actions/player";
 import React from "react";
-import { Image, Text, View, Animated, PanResponder, ScrollView, Slider, FlatList, TouchableHighlight} from "react-native";
+import { Image, Text, View, Animated, PanResponder, ScrollView, Slider, FlatList} from "react-native";
 import Sound from 'react-native-sound';
 import Dimensions from 'Dimensions';
 import { BlurView } from 'react-native-blur';
-import {ListItem, Left, Icon, Right, Title, Body } from "native-base";
+import {ListItem, Left, Icon, Right, Title } from "native-base";
 import {API_REQUEST_SONG_GET, API_REQUEST_SONG_RELATION, API_REQUEST_SONG_LYRIC} from "../actions/api";
 import PlaylistTouchableBtn from "../components/playListTouchableBtnComponent"
 import {keyFromAction} from "../lib/action_utilities";
@@ -37,7 +37,7 @@ class Player extends React.Component {
         };
     }
     componentDidMount(){
-        this.props.loadRelationSong('6DHBZXxtNIKG');
+
     }
     componentWillMount() {
         this._panResponder = PanResponder.create({
@@ -196,9 +196,9 @@ class Player extends React.Component {
                         <Text>{props.song.artistName}</Text>
                     </View>
                     <View style={{right: 8, justifyContent: 'center', flexDirection: 'row', }}>
-                        {props.isPlaying ? (<PlaylistTouchableBtn img={'play'} onClick={props.pause}/>) : (
-                            <PlaylistTouchableBtn img={'pause'} onClick={props.play}/>)}
-                        <PlaylistTouchableBtn img={'next'} onClick={props.next} style={{left: 8}}/>
+                        {props.isPlaying ? (<PlaylistTouchableBtn size={46} img={'play'} onClick={props.pause}/>) : (
+                            <PlaylistTouchableBtn size={36} img={'pause'} onClick={props.play}/>)}
+                        <PlaylistTouchableBtn size={36} img={'next'} onClick={props.next} style={{left: 8}}/>
                     </View>
                 </View>
 
@@ -230,11 +230,11 @@ class Player extends React.Component {
                     </View>
                     <View style={{position:'absolute', bottom: 0, width: '100%', height: '24%'}}>
                         <View style={{display:'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-                            <PlaylistTouchableBtn img={'like'} style={{width:36, height: 36}}/>
-                            <PlaylistTouchableBtn img={'like'} style={{width:36, height: 36}}/>
-                            <PlaylistTouchableBtn img={'like'} style={{width:36, height: 36}}/>
-                            <PlaylistTouchableBtn img={'like'} style={{width:36, height: 36}}/>
-                            <PlaylistTouchableBtn img={'more'} style={{width:36, height: 36}}/>
+                            <PlaylistTouchableBtn size={36} img={'like'} style={{width:36, height: 36}}/>
+                            <PlaylistTouchableBtn size={36} img={'like'} style={{width:36, height: 36}}/>
+                            <PlaylistTouchableBtn size={36} img={'like'} style={{width:36, height: 36}}/>
+                            <PlaylistTouchableBtn size={36} img={'like'} style={{width:36, height: 36}}/>
+                            <PlaylistTouchableBtn size={36} img={'more'} style={{width:36, height: 36}}/>
                         </View>
                         <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', alignContent: 'center', justifyContent: 'center'}}>
                             <Text style={{color:'white'}}>00:00</Text>
@@ -242,20 +242,22 @@ class Player extends React.Component {
                             <Text style={{color:'white'}}>00:00</Text>
                         </View>
                         <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent:'center'}}>
-                            <PlaylistTouchableBtn img={'order'} style={{width:46, height: 46}}/>
-                            <PlaylistTouchableBtn img={'prev'} style={{width:46, height: 46, marginLeft: 8}}/>
+                            <PlaylistTouchableBtn size={46} img={'order'} style={{width:46, height: 46}}/>
+                            <PlaylistTouchableBtn size={46} img={'prev'} style={{width:46, height: 46, marginLeft: 8}}/>
                             { props.isPlaying ? (
                                 <PlaylistTouchableBtn
+                                    size={56}
                                     img={'pause'} style={{width:56, height: 56, marginLeft: 8, marginRight: 8}}
                                     onClick={()=>this.props.pause()}/>
                             ): (
                                 <PlaylistTouchableBtn
+                                    size={56}
                                     img={'play'} style={{width:56, height: 56, marginLeft: 8, marginRight: 8}}
                                     onClick={()=>{
                                         this.props.loadSong('6DHBZXxtNIKG')}}/>
                             )}
-                            <PlaylistTouchableBtn img={'next'} style={{width:46, height: 46, marginRight: 8}}/>
-                            <PlaylistTouchableBtn img={'list'} style={{width:46, height: 46}}/>
+                            <PlaylistTouchableBtn size={46} img={'next'} style={{width:46, height: 46, marginRight: 8}}/>
+                            <PlaylistTouchableBtn size={46} img={'list'} style={{width:46, height: 46}}/>
                         </View>
                     </View>
                 </View>
@@ -320,13 +322,11 @@ export default connect((state, ownProps) => {
     next: () => dispatch(PLAYER_NOWLIST_NEXT()),
     toggleView: () => dispatch(PLAYER_TOGGLE()),
     loadSong: (songId) => {
+        dispatch(API_REQUEST_SONG_RELATION(songId));
         dispatch(API_REQUEST_SONG_LYRIC(songId));
         dispatch(API_REQUEST_SONG_GET(songId));
         dispatch(PLAYER_NOWLIST_CLEAR());
         dispatch(PLAYER_NOWLIST_ADD(songId));
         dispatch(PLAYER_PLAY());
-    },
-    loadRelationSong:(songId)=>{
-        dispatch(API_REQUEST_SONG_RELATION(songId));
     }
 }))(Player)
